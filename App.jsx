df4cdc215a46f804bc9ce339bd067a23f68f86f7
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-05-20 13:11:59 +0530

    update

--- App.jsx
+++ App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div className="container">
 
       <div className="card1">
-        <Card name="Rajalaxmi" age={25} onclick={handelClick}/>
+        <Card name="Rajalaxmi" age={25} onclick={handelClick} src ="https://media.licdn.com/dms/image/v2/D5603AQGotsNPHBAp4g/profile-displayphoto-crop_800_800/B56Z4w.I_tIQAM-/0/1778938057638?e=1781136000&v=beta&t=oxbdv3T6BpR0xMrbsNPpQ8oe7eGRKqFGAXaE1BKHTL0"/>
       </div>
 
       <div className="card2">
